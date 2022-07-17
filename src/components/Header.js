ff4580f2f283ef88d33c0585a2c2935829892b31
samy884 <ssgill884@gmail.com>
2022-07-17 22:06:32 +0530

    adding badge

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,25 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Badge from "@mui/material/Badge";
 
 const Header = () => {
   return (
     <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">AddToCart</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <i class="fa-solid fa-cart-shopping"></i>
-          </Nav>
-        </Container>
-      </Navbar>
-  )
-}
+      <Container>
+        <Navbar.Brand href="#home">AddToCart</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+        </Nav>
+        <Badge badgeContent={0} color="primary">
+          <i
+            class="fa-solid fa-cart-shopping text-light"
+            style={{ fontSize: 25, cursor: "pointer" }}
+          ></i>
+        </Badge>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default Header
+export default Header;
