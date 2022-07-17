093f6e8e488699897539fc9a84adc33b7138f6a8
samy884 <ssgill884@gmail.com>
2022-07-17 21:55:32 +0530

    cart icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,10 @@ const Header = () => {
   return (
     <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">AddToCart</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <i class="fa-solid fa-cart-shopping"></i>
           </Nav>
         </Container>
       </Navbar>
